3eed29791ca36e7ca3c5f8b626e6c7005a3b1875
Chuan Miao <chuan137@gmail.com>
2016-01-28 19:34:53 +0100

    init

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name = 'pypipe',
+    version = '0.0.1',
+    author = 'Chuan Miao',
+    author_mail = 'chuan137@gmail.com',
+    description = 'write lightweight coroutine pipelines in python',
+    license = 'GNU',
+    keyword = 'coroutine pipeline',
+    packages = ['pypipe']
+)
