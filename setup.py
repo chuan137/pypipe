820006e126c52ccd88b01c006a7cf52d6bf0591c
Chuan Miao <chuan137@gmail.com>
2016-01-29 11:54:38 +0100

    Version 0.0.2

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
+import os
 from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name = 'pypipe',
-    version = '0.0.1',
+    version = read('VERSION'),
     author = 'Chuan Miao',
     author_mail = 'chuan137@gmail.com',
     description = 'write lightweight coroutine pipelines in python',
